91fa850e5c6e9aaa88df06f85b8f3b09494665a4
HJianFei <190766172@qq.com>
2018-02-24 13:51:38 +0800

    票圈顶部选择按钮

--- app/src/main/java/com/hjianfei/rover/view/fragment/circle/CircleFragment.java
+++ app/src/main/java/com/hjianfei/rover/view/fragment/circle/CircleFragment.java
@@ -6,14 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.hjianfei.rover.R;
+import com.hjianfei.rover.utils.ToastUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import co.ceryle.radiorealbutton.RadioRealButton;
+import co.ceryle.radiorealbutton.RadioRealButtonGroup;
 
 
 public class CircleFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.iv_person_avatar)
+    ImageView ivPersonAvatar;
+    @BindView(R.id.button_group_bt)
+    RadioRealButtonGroup buttonGroup;
+    @BindView(R.id.toolbar)
+    LinearLayout toolbar;
+    @BindView(R.id.appbar)
+    LinearLayout appbar;
+    Unbinder unbinder;
 
     private String mParam1;
     private String mParam2;
@@ -44,7 +62,20 @@ public class CircleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_circle, container, false);
+        View view = inflater.inflate(R.layout.fragment_circle, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        buttonGroup.setOnClickedButtonListener(new RadioRealButtonGroup.OnClickedButtonListener() {
+            @Override
+            public void onClickedButton(RadioRealButton button, int position) {
+                ToastUtil.showToast(getContext(),button.getText().toString());
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
